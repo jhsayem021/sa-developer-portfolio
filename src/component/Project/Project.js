2eb4d5f4fa27bdc57a3b22431137e86e0060353f
nothing021 <92251653+nothing021@users.noreply.github.com>
2021-12-04 00:04:49 +0600

    upadate url link2

--- src/component/Project/Project.js
+++ src/component/Project/Project.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 const Project = (props) => {
     const {id,image1,title,description} = props.project;
