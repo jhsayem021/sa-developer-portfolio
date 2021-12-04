1f7d0d49201318b2bf9ae99416c6a021e9ff3033
nothing021 <92251653+nothing021@users.noreply.github.com>
2021-12-04 15:37:48 +0600

    project add2

--- src/component/Navber/Navber.js
+++ src/component/Navber/Navber.js
@@ -3,13 +3,41 @@ import './Navber.css'
 import { Link } from 'react-router-dom';
 const Navber = () => {
     return (
-        <div className="navber">
-            <ul className="flex navlink">
-                <li> <Link className="nav  px-2 py-1 rounded ms-2" to="/home">Home</Link> </li>
-                <li> <Link className="nav  px-2 py-1 rounded ms-2" to="/projects">Projects</Link> </li>
-                <li> <Link className="nav  px-2 py-1 rounded ms-2" to="/about">About me</Link> </li>
-                <li> <Link className="nav  px-2 py-1 rounded ms-2" to="/contacts">Contact me</Link> </li>
-            </ul>
+        <div>
+        {/* <!-- This example requires Tailwind CSS v2.0+ --> */}
+<nav className="bg-gray-800 py-2">
+  <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+    <div className="relative flex items-center justify-between">
+      <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+        {/* <!-- Mobile menu button--> */}
+      </div>
+      <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+        <div className="hidden sm:block sm:ml-6">
+          <div className="flex space-x-4 navlink">
+            {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+             <Link className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" to="/home">Home</Link> 
+                 <Link className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" to="/projects">Projects</Link> 
+                 <Link className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" to="/about">About me</Link> 
+                 <Link className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" to="/contacts">Contact me</Link> 
+          </div>
+        </div>
+      </div>
+    
+    </div>
+  </div>
+
+  {/* <!-- Mobile menu, show/hide based on menu state. --> */}
+  <div className="sm:hidden" id="mobile-menu">
+    <div className=" pt-2 pb-3 space-y-1 navlink">
+      {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+      <Link className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" to="/home">Home</Link> 
+                 <Link className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" to="/projects">Projects</Link> 
+                 <Link className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" to="/about">About me</Link> 
+                 <Link className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" to="/contacts">Contact me</Link> 
+    </div>
+  </div>
+</nav>
+
         </div>
     );
 };
